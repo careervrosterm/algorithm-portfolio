03229ed48b33fa8019b0a3b53cdf40fa3d854ac4
vrosterm <vrosterm@calpoly.edu>
2026-07-07 00:57:24 -0700

    Fixing code where np.matmult should have been np.dot, and fixing the predictor as to give a proper n x 1 array of y values.

--- Local Housing Analysis/linear_regression.py
+++ Local Housing Analysis/linear_regression.py
@@ -3,22 +3,21 @@ from metrics import *
 
 def fit(X,Y,stepsize,metric: str): 
         i = 0
-        n = len(X)
+        samples, features = X.shape
         # Begin with the guess of 0 for the coefficients and 0 for the intercept.
-        coeff = np.zeros(n)
+        coeff = np.zeros(features)
         intercept = 0
         # Calculate first y_bar output
-        theta_i = np.matmul(coeff,X) + intercept
+        theta_i = np.dot(X,coeff) + intercept
         error = error_metric(metric) #One of four metrics from the metrics.py file
-        while error.error(Y,theta_i) < 1e-8 or i <= 50000:
-            D_c = error.D_c(n,X,Y,theta_i)  # Derivative wrt coeff
-            D_i = error.D_i(n,Y,theta_i)  # Derivative wrt i
+        while error.error(Y,theta_i) > 1e-8 and i <= 50000:
+            D_c = error.D_c(samples,X,Y,theta_i)  # Derivative wrt coeff
+            D_i = error.D_i(samples,Y,theta_i)  # Derivative wrt i
             coeff = coeff - stepsize * D_c  # Update coeff
             intercept = intercept - stepsize * D_i  # Update intercept
-            theta_i = np.matmul(coeff,X) + intercept #Solve for y using the current iteration guess.
+            theta_i = np.dot(X,coeff) + intercept #Solve for y using the current iteration guess.
             i += 1
-        err = error.error(Y,theta_i)
-        return coeff, intercept, err
+        return coeff, intercept
 
 def error_metric(metric):
     class_list = {
@@ -27,6 +26,10 @@ def error_metric(metric):
         "RMSE": RMSE()
     }
     return class_list[metric.upper()] 
+
+def predict(X, coeff, intercept):
+    return ((X.T*coeff) + intercept)
+     
 
 '''
 def backtrack(metric): #Backtracking along current guessing path to adapt step size
--- Local Housing Analysis/metrics.py
+++ Local Housing Analysis/metrics.py
@@ -3,25 +3,25 @@ import numpy as np
 '''This could probably all be conglomerated into one big "error" class whose instance changes the variables, but considering there are only three, this is sufficient.'''
 class MSE():
     def D_c(self,n,X,Y,theta_i):
-        return -(2/n) * X*(Y - theta_i)
+        return -(2/n) * np.dot(X.T,(Y - theta_i))
     def D_i(self,n,Y,theta_i):
-        return -(2/n) * (Y - theta_i)
+        return -(2/n) * np.sum(Y - theta_i)
     def error(self,y, y_hat):
         return np.mean((y - y_hat)**2)
 
 class RMSE():
     def D_c(self,n,X,Y,theta_i):
-        return -X*(Y-theta_i)/(n*RMSE.error(self, Y, theta_i))
+        return -np.dot(X.T,(Y-theta_i))/(n*RMSE.error(self, Y, theta_i))
     def D_i(self,n,Y,theta_i):
-        return (Y-theta_i)/(n*RMSE.error(self, Y, theta_i))
+        return np.sum(Y-theta_i)/(n*RMSE.error(self, Y, theta_i))
     def error(self, y, y_hat):
         return np.sqrt(np.mean((y - y_hat)**2))
     
 class MAE():
     def D_c(self,n,X,Y,theta_i):
-        return -(1/n) * X * np.sign(Y - theta_i)
+        return -(1/n) * np.dot(X.T, np.sign(Y - theta_i))
     def D_i(self,n,Y,theta_i):
-        return -(1/n) * np.sign(Y - theta_i)
+        return -(1/n) * np.sum(np.sign(Y - theta_i))
     def error(self, y, y_hat):
         return np.mean(np.abs(y - y_hat))
 
